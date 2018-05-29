02910851286210bdcae46c787bbb0b14be18ab42
Naruto TAKAHASHI <tnaruto@gmail.com>
2018-05-29 22:34:42 +0900

    Revert "rename bitcoind to xpchaind"
    This reverts commit 93609321c8c5509e383d525cf9efd1e86f2316e9.

--- src/noui.cpp
+++ src/noui.cpp
@@ -51,7 +51,7 @@ static void noui_InitMessage(const std::string& message)
 
 void noui_connect()
 {
-    // Connect xpchaind signal handlers
+    // Connect bitcoind signal handlers
     uiInterface.ThreadSafeMessageBox.connect(noui_ThreadSafeMessageBox);
     uiInterface.ThreadSafeQuestion.connect(noui_ThreadSafeQuestion);
     uiInterface.InitMessage.connect(noui_InitMessage);
